0ccca9831dba0761404122b5369732a2a62a729c
Thomas Krüger <contact@thkru.com>
2018-10-11 13:46:56 +0200

    made constraint layout work!

--- app/src/androidTest/java/com/thkrue/cert/LoginTest.java
+++ app/src/androidTest/java/com/thkrue/cert/LoginTest.java
@@ -13,6 +13,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -25,13 +26,16 @@ public class LoginTest {
 
     @Test
     public void testLogin() {
-        
+
         onView(withId(R.id.et_email)).perform(typeText("android@thkru.com"));
         onView(withId(R.id.et_password)).perform(typeText("android"));
         onView(withId(R.id.btn_login)).perform(click());
 
         onView(withId(R.id.tv_info)).check(matches(withText(("Logged in!"))));
         onView(withId(R.id.tv_info)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+        onView(withText("Login")).check(matches(isDisplayed()));
+
     }
 
 }
