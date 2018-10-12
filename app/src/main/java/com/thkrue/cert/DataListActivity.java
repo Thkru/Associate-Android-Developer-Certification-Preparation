60a59dd1384f9bd1d1576d9c26291905a20e377b
Thomas Krüger <contact@thkru.com>
2018-10-12 14:43:19 +0200

    VM Providers working and changing to AndroidVM

--- app/src/main/java/com/thkrue/cert/DataListActivity.java
+++ app/src/main/java/com/thkrue/cert/DataListActivity.java
@@ -1,6 +1,7 @@
 package com.thkrue.cert;
 
 import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -32,7 +33,7 @@ public class DataListActivity extends AppCompatActivity {
     }
 
     private void setupViewModel(final MyListAdapter adapter) {
-        MyEntityViewModel myEntityVM = new MyEntityViewModel(this.getApplicationContext());
+        MyEntityViewModel myEntityVM = ViewModelProviders.of(this).get(MyEntityViewModel.class);
         myEntityVM.getAllEntities().observe(this, new Observer<List<MyEntity>>() {
             @Override
             public void onChanged(@Nullable final List<MyEntity> entities) {
--- app/src/main/java/com/thkrue/cert/room/MyEntityViewModel.java
+++ app/src/main/java/com/thkrue/cert/room/MyEntityViewModel.java
@@ -1,18 +1,19 @@
 package com.thkrue.cert.room;
 
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.ViewModel;
-import android.content.Context;
 
 import java.util.List;
 
-public class MyEntityViewModel extends ViewModel {
+public class MyEntityViewModel extends AndroidViewModel {
 
     private MyEntityRepository mRepo;
     private LiveData<List<MyEntity>> mAllEntities;
 
-    public MyEntityViewModel(Context c) {
-        mRepo = new MyEntityRepository(c);
+    public MyEntityViewModel(Application a) {
+        super(a);
+        mRepo = new MyEntityRepository(a.getApplicationContext());
         mAllEntities = mRepo.getData();
     }
 
